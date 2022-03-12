7e8d9ed6dd6f1f609e456fe1a91e860c85bddebe
punkmachine <alex_rassydihin@mail.ru>
2022-03-12 10:01:03 +0600

    add cardList mobile adaptation.

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col, Card, Typography } from 'antd';
 import { CardLesson } from '../components/cardLesson';
 
 function MainPage() {
+	const [stateSpan, setStateSpan] = useState(6);
+
 	const array = [
 		{
 			title: 'Английский язык'
@@ -30,12 +32,22 @@ function MainPage() {
 		},
 	];
 
+	useEffect(() => {
+		const screenWidth = window.screen.width;
+
+		if (screenWidth <= 1024 && screenWidth > 768) {
+			setStateSpan(8);
+		} else if (screenWidth <= 768 && screenWidth > 425) {
+			setStateSpan(12);
+		} else if (screenWidth <= 425) setStateSpan(24);
+	}, []);
+
 	return (
 		<div className='cardList'>
 			<Typography.Title level={1} style={{ textAlign: 'center', margin: '15px 0' }}>Предметный лист</Typography.Title>
 			<Row gutter={[20, 20]}>
 				{array.map((item, index) =>
-					<Col span={6} key={index}>
+					<Col span={stateSpan} key={index}>
 						<CardLesson {...item} />
 					</Col>
 				)}
