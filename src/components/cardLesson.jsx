b3a994a887737a1ae81acccf96645757b8c2dc35
punkmachine <alex_rassydihin@mail.ru>
2022-03-15 09:02:40 +0600

    add card lesson click.

--- src/components/cardLesson.jsx
+++ src/components/cardLesson.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Typography } from 'antd';
+import { useRedirect } from '../hooks/redirect';
 
 function CardLesson(props) {
 	const { id, title } = props;
+	const { goLesson } = useRedirect();
 
 	return (
-		<div className="card-lesson-wrapper">
+		<div className="card-lesson-wrapper" onClick={() => goLesson(id)}>
 			<Typography.Title level={5}>{title}</Typography.Title>
 		</div>
 	);
