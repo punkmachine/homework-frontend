bcaa83cc707c9d57b77796b1afa4e6fc515e87cb
punkmachine <alex_rassydihin@mail.ru>
2022-04-10 06:43:12 +0600

    fix helmet.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Helmet } from "react-helmet";
+import { useLocation } from 'react-router-dom';
 
 import { MainPage } from './pages/main';
 import { LessonPage } from './pages/lesson';
 import { SchedulePage } from './pages/schedule';
-
-import { usePathname } from './hooks/pathname';
+import { Login } from './pages/login';
+import { Registration } from './pages/registration';
 
 import { Header } from './components/header';
 
-import { LESSON_PAGE_PATH, SCHEDULE_PAGE_PATH } from './constants/routes';
+import { LESSON_PAGE_PATH, SCHEDULE_PAGE_PATH, LOGIN_PAGE_PATH, REGISTRATION_PAGE_PATH } from './constants/routes';
 import { headData } from './constants/helmet';
 
 function App() {
-	const { clearPath } = usePathname();
+	const { pathname } = useLocation();
 
 	return (
 		<div className='App'>
 			<Helmet>
-				<title>{headData[clearPath].title ?? ''}</title>
-				<meta name='description' content={`${headData[clearPath].description ?? ''}`} />
+				<title>{headData[pathname]?.title ?? ''}</title>
+				<meta name='description' content={`${headData[pathname]?.description ?? ''}`} />
 			</Helmet>
 
 			<Header />
 			<main className="content">
 				<Switch>
 					<Route exact path='/' component={MainPage} />
+					<Route path={LOGIN_PAGE_PATH} component={Login} />
+					<Route path={REGISTRATION_PAGE_PATH} component={Registration} />
 					<Route path={`${LESSON_PAGE_PATH}-:id`} component={LessonPage} />
 					<Route path={SCHEDULE_PAGE_PATH} component={SchedulePage} />
 				</Switch>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Menu, Typography } from 'antd';
 import { LeftCircleOutlined } from '@ant-design/icons';
 
 import { useRedirect } from '../hooks/redirect';
-import { usePathname } from '../hooks/pathname';
 
 import { SCHEDULE_PAGE_PATH, MAIN_PAGE_PATH } from '../constants/routes';
 
 function Header() {
 	const { goback } = useRedirect();
-	const { pathname } = usePathname();
+	const { pathname } = useLocation();
 
 	const [hidden, setHidden] = useState(true);
 
