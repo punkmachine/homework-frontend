2ed8e09475d8466d60a58142e81ecb740f692993
punkmachine <alex_rassydihin@mail.ru>
2022-04-30 12:58:32 +0600

    add authSlice.

---
+++ src/redux/authSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	jwt: null,
+	user: null,
+	isAuth: false,
+};
+
+export const authSlice = createSlice({
+	name: 'authSlice',
+	initialState,
+	reducers: {
+		loginAction: (state, action) => {
+			const { payload } = action;
+			const { id, jwt, name } = payload;
+
+			const newUser = {
+				name, id
+			}
+
+			state.user = { ...newUser };
+			state.jwt = jwt;
+			localStorage.setItem('user', JSON.stringify(newUser));
+			state.isAuth = true;
+		},
+		logoutAction: (state) => {
+			console.log('done');
+			state = { ...initialState };
+
+			console.log(state);
+		},
+		refeachState: (state) => {
+			let cookieJWT = document.cookie
+				.split(';')
+				.map(item => {
+					const index = item.indexOf('=');
+					const key = item.slice(0, index);
+					const value = item.slice(index + 1, item.length);
+
+					return {
+						[key]: value,
+					};
+				})
+				.filter(item => item.JWT);
+
+			state.jwt = cookieJWT[0]?.JWT ?? null;
+			state.user = JSON.parse(localStorage.getItem('user'));
+			state.isAuth = !!state.jwt;
+		}
+	}
+});
+
+export const authReducer = authSlice.reducer;
+export const { loginAction, logoutAction, refeachState } = authSlice.actions;
