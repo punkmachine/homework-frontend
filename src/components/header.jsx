1ca88c20e22534c3c20b287c6dafd8dec278ac2a
punkmachine <alex_rassydihin@mail.ru>
2022-05-15 12:24:17 +0600

    add mobile adaptation menu.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu, Typography, Button } from 'antd';
-import { LeftCircleOutlined } from '@ant-design/icons';
+import { LeftCircleOutlined, MenuOutlined, CloseOutlined } from '@ant-design/icons';
 
 import { logoutAction, refeachState } from '../redux/authSlice';
 import { useRedirect } from '../hooks/redirect';
@@ -19,12 +19,11 @@ function Header() {
 	const { CookiesDelete } = useCookies();
 
 	const [hidden, setHidden] = useState(true);
-	const [activeKey, setActiveKey] = useState('main');
+	const [mobileMenu, setMobileMenu] = useState(false);
+	const [showMobileMenuContent, setShowMobileMenuContent] = useState(false);
 
-	function selectMenu(key) {
-		setActiveKey(key);
-		localStorage.setItem('activeKey', key);
-	}
+	const selectMenu = (key) => localStorage.setItem('activeKey', key);
+	const toggleMobileMenu = () => setShowMobileMenuContent((prevState) => !prevState);
 
 	function clear() {
 		CookiesDelete();
@@ -35,51 +34,97 @@ function Header() {
 
 	useEffect(() => {
 		setHidden(pathname === MAIN_PAGE_PATH);
+		setShowMobileMenuContent(false);
 	}, [pathname]);
 
 	useEffect(() => {
-		setActiveKey(localStorage.getItem('activeKey') ?? 'main');
 		dispatch(refeachState());
+		setMobileMenu(window.screen.width <= 425);
 	}, []);
+
+	console.log(mobileMenu);
 
 	return (
 		<header className="header">
-			<div className='back' onClick={goback} hidden={hidden}>
-				<LeftCircleOutlined className='back-icon' />
-				<Typography>Назад</Typography>
-			</div>
-			<nav className="menu">
-				<span
-					className={activeKey === 'main' ? "menu-item active" : "menu-item"}
-					onClick={() => selectMenu('main')}
-				>
-					<Link to='/'>
-						Главная
-					</Link>
-				</span>
-				<span
-					className={activeKey === 'schedule' ? "menu-item active" : "menu-item"}
-					onClick={() => selectMenu('schedule')}
-				>
-					<Link to={SCHEDULE_PAGE_PATH}>
-						Расписание
-					</Link>
-				</span>
-			</nav>
-			<div className="auth-controller">
-				{isAuth
-					? (
-						<>
-							<Button type="link" onClick={clear}>Выход</Button>
-						</>
-					)
-					: (
-						<>
-							<Button type="link" onClick={goLogin}>Вход</Button>
-							<Button type="link" onClick={goReg}>Регистрация</Button>
-						</>
-					)}
-			</div>
+			{mobileMenu ?
+				<div>
+					<MenuOutlined className='hamburger' onClick={toggleMobileMenu} />
+					<div className="mobile-menu-content" hidden={!showMobileMenuContent}>
+						<CloseOutlined className='close-menu' onClick={toggleMobileMenu} />
+						<nav className="mobile-menu-nav" hidden={!showMobileMenuContent}>
+							<span
+								className={pathname === '/' ? "menu-item active" : "menu-item"}
+								onClick={() => selectMenu('/')}
+							>
+								<Link to='/'>
+									Главная
+								</Link>
+							</span>
+							<span
+								className={pathname === SCHEDULE_PAGE_PATH ? "menu-item active" : "menu-item"}
+								onClick={() => selectMenu(SCHEDULE_PAGE_PATH)}
+							>
+								<Link to={SCHEDULE_PAGE_PATH}>
+									Расписание
+								</Link>
+							</span>
+						</nav>
+						<div className="mobile-menu-auth" hidden={!showMobileMenuContent}>
+							{isAuth
+								? (
+									<>
+										<Button type="link" onClick={clear}>Выход</Button>
+									</>
+								)
+								: (
+									<>
+										<Button type="link" onClick={goLogin}>Вход</Button>
+										<Button type="link" onClick={goReg}>Регистрация</Button>
+									</>
+								)}
+						</div>
+					</div>
+					<div className="hz" hidden={!showMobileMenuContent} onClick={toggleMobileMenu} />
+				</div>
+				: (<div>
+					<div className='back' onClick={goback} hidden={hidden}>
+						<LeftCircleOutlined className='back-icon' />
+						<Typography>Назад</Typography>
+					</div>
+					<nav className="menu">
+						<span
+							className={pathname === '/' ? "menu-item active" : "menu-item"}
+							onClick={() => selectMenu('/')}
+						>
+							<Link to='/'>
+								Главная
+							</Link>
+						</span>
+						<span
+							className={pathname === SCHEDULE_PAGE_PATH ? "menu-item active" : "menu-item"}
+							onClick={() => selectMenu(SCHEDULE_PAGE_PATH)}
+						>
+							<Link to={SCHEDULE_PAGE_PATH}>
+								Расписание
+							</Link>
+						</span>
+					</nav>
+					<div className="auth-controller">
+						{isAuth
+							? (
+								<>
+									<Button type="link" onClick={clear}>Выход</Button>
+								</>
+							)
+							: (
+								<>
+									<Button type="link" onClick={goLogin}>Вход</Button>
+									<Button type="link" onClick={goReg}>Регистрация</Button>
+								</>
+							)}
+					</div>
+				</div>)
+			}
 		</header>
 	);
 }
