7b1e66d6ba6cc92a6d7b53048a0128fcc40f2988
punkmachine <alex_rassydihin@mail.ru>
2022-05-20 06:04:29 +0600

    add exams and profile pages routes.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import { LessonPage } from './pages/LessonPage';
 import { SchedulePage } from './pages/SchedulePage';
 import { Login } from './pages/LoginPage';
 import { Registration } from './pages/RegistrationPage';
+import { ExamsPage } from './pages/ExamsPage';
+import { ProfilePage } from './pages/ProfilePage';
 
 import { Header } from './components/app/Header';
 
-import { LESSON_PAGE_PATH, SCHEDULE_PAGE_PATH, LOGIN_PAGE_PATH, REGISTRATION_PAGE_PATH } from './constants/routes';
+import {
+	LESSON_PAGE_PATH, SCHEDULE_PAGE_PATH, LOGIN_PAGE_PATH, REGISTRATION_PAGE_PATH, EXAMS_PAGE_PATH, PROFILE_PAGE_PATH
+} from './constants/routes';
 import { headData } from './constants/helmet';
 
 function App() {
@@ -30,8 +34,10 @@ function App() {
 					<Route exact path='/' component={MainPage} />
 					<Route path={LOGIN_PAGE_PATH} component={Login} />
 					<Route path={REGISTRATION_PAGE_PATH} component={Registration} />
-					<Route path={`${LESSON_PAGE_PATH}-:id`} component={LessonPage} />
 					<Route path={SCHEDULE_PAGE_PATH} component={SchedulePage} />
+					<Route path={EXAMS_PAGE_PATH} component={ExamsPage} />
+					<Route path={PROFILE_PAGE_PATH} component={ProfilePage} />
+					<Route path={`${LESSON_PAGE_PATH}-:id`} component={LessonPage} />
 				</Switch>
 			</main>
 		</div>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,3 +3,5 @@ export const SCHEDULE_PAGE_PATH = '/schedule';
 export const LESSON_PAGE_PATH = '/lesson';
 export const LOGIN_PAGE_PATH = '/login';
 export const REGISTRATION_PAGE_PATH = '/registration';
+export const PROFILE_PAGE_PATH = '/profile';
+export const EXAMS_PAGE_PATH = '/exams';
